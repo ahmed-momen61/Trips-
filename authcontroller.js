b8ea9ad6898c70a2e798a4be2e0bbc8375068477
ahmed-momen61 <aa2301532@tkh.edu.eg>
2025-11-04 10:47:58 +0200

    bcrypt

---
+++ authcontroller.js
@@ -0,0 +1,30 @@
+const { db } = require('../db.js')
+
+
+const signup = (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const role = 'user'; // default to ron-admin
+
+    if (!name || !email || !password) {
+        return res.status(400).send('Please provide name, email, and password')
+    }
+
+    const query = `
+        INSERT INTO USER (NAME, EMAIL, ROLE, PASSWORD) 
+        VALUES ('${name}', '${$email}', '${role}', '${password}')
+`;
+
+    db.run(query, (err) => {
+        if (err) {
+            console.log(err.message);
+            if (err.message.includes('UNIQUE constraint')) {
+                return res.status(400).send('Email already in use');
+            }
+            return res.status(500).send('database error');
+        }
+
+        return res.status(200).send('registration successful');
+    });
+};
